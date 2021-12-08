81ebfd10c94e8f14a73894a0c05daa58fbe448c4
Ovidiu Turcu <ovi_tsb@yahoo.com>
2021-12-08 16:11:55 -0500

    Dev tool initial commit

---
+++ lib/ovi_view_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require_relative "ovi_view_tool/version"
+require_relative "ovi_view_tool/renderer"
+
+module OviViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ovi_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OviViewTool
+  VERSION = "0.1.0"
+end
